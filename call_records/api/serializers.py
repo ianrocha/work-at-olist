f3093bd260996a73685fdd17f566ef753b90c4e0
Ian Rocha <ianbritosr@gmail.com>
2018-07-10 15:21:08 -0300

    Add validators to CallRecordSerializer

--- call_records/api/serializers.py
+++ call_records/api/serializers.py
@@ -6,6 +6,14 @@ from call_records.models import CallRecord
 
 class CallRecordSerializer(ModelSerializer):
     class Meta:
+        validators = [
+            UniqueTogetherValidator(
+                queryset=CallRecord.objects.all(),
+                fields=('call_id', 'record_type', 'phone_source'),
+                message='The record call_id + record_type already exists to this phone source'
+            )
+        ]
+
         model = CallRecord
         fields = ('id', 'call_id', 'phone_source', 'phone_destination', 'record_type', 'record_timestamp')
 
