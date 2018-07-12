72f56fd2fcc6a68f77ca44cd2532233780c80972
Ian Rocha <ianbritosr@gmail.com>
2018-07-12 10:00:45 -0300

    Change TeleBillSerializer back to ModelSerializer

--- telephone_bill/api/viewsets.py
+++ telephone_bill/api/viewsets.py
@@ -1,8 +1,8 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
 from telephone_bill.models import TelephoneBill
 from .serializers import TelephoneBillSerializer
 
 
-class TelephoneBillViewSet(ReadOnlyModelViewSet):
+class TelephoneBillViewSet(ModelViewSet):
     serializer_class = TelephoneBillSerializer
     queryset = TelephoneBill.objects.all()
