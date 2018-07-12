5cd688661bf236541a2f5795854ad72659a4dd4c
Ian Rocha <ianbritosr@gmail.com>
2018-07-12 10:28:48 -0300

    Add TokenAuthentication to TelephoneBillViewSet

--- telephone_bill/api/viewsets.py
+++ telephone_bill/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from telephone_bill.models import TelephoneBill
@@ -5,6 +6,7 @@ from .serializers import TelephoneBillSerializer
 
 
 class TelephoneBillViewSet(ModelViewSet):
+    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticatedOrReadOnly,)
     serializer_class = TelephoneBillSerializer
     queryset = TelephoneBill.objects.all()
