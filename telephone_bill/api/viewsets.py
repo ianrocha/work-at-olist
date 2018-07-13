648e85814992b73df9c44a8d61d5e072774a76cd
Ian Rocha <ianbritosr@gmail.com>
2018-07-13 14:58:04 -0300

    Override the list() method for TelephoneBillViewSet

--- telephone_bill/api/viewsets.py
+++ telephone_bill/api/viewsets.py
@@ -1,6 +1,7 @@
 import datetime
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from telephone_bill.models import TelephoneBill
 from .serializers import TelephoneBillSerializer
@@ -12,6 +13,9 @@ class TelephoneBillViewSet(ModelViewSet):
     serializer_class = TelephoneBillSerializer
 
     def get_queryset(self):
+        """
+        :return: Queryset filtered by source number and period
+        """
         source_phone = self.request.query_params.get('source', None)
         bill_period = self.request.query_params.get('period', None)
 
@@ -24,3 +28,21 @@ class TelephoneBillViewSet(ModelViewSet):
                 bill_period = str(month) + '-' + str(year)
 
             return queryset.filter(period__exact=bill_period).order_by('start_date', 'start_time')
+
+    def list(self, request, *args, **kwargs):
+        """
+        List a queryset if a source number is passed or pass a message
+        """
+        source = self.request.query_params.get('source', None)
+        if source is not None:
+            queryset = self.filter_queryset(self.get_queryset())
+
+            page = self.paginate_queryset(queryset)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
+        else:
+            return Response({'Message': 'You need to pass the source number. For more info go to documentation'})
