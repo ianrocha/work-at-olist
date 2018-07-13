a89c6777bfce5c0ee3c4bb11bba9a4177a4826c2
Ian Rocha <ianbritosr@gmail.com>
2018-07-13 14:02:10 -0300

    Change Period from DateField to CharField and create a regexvalidator

--- telephone_bill/models.py
+++ telephone_bill/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class TelephoneBill(models.Model):
     # Simple Regex Expression for phone number validation
     phone_validator = RegexValidator(regex=r'^((10)|([1-9][1-9]))\d{8,9}$')
+    period_validator = RegexValidator(regex=r'^(10|11|12|[1-9])[-]\d{4}$')
 
     source = models.CharField(max_length=11, blank=True, null=True, validators=[phone_validator])
     destination = models.CharField(max_length=11, blank=True, null=True, validators=[phone_validator])
@@ -12,5 +13,7 @@ class TelephoneBill(models.Model):
     start_date = models.DateField()
     duration = models.DurationField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    # TODO: Try to change period format to month-year
-    period = models.DateField()
+    period = models.CharField(max_length=7, validators=[period_validator])
+
+    def __str__(self):
+        return 'Source: {} Period: {}'.format(self.source, self.period)
