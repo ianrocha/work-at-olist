cccd65afa272032169a2f8cc3162174f6dc900b2
Ian Rocha <ianbritosr@gmail.com>
2018-07-17 10:07:30 -0300

    Configuring the app

--- record_keeper/settings.py
+++ record_keeper/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
