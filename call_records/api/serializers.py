1a8b74122985247f0a66bd22052a9e2f2fad3250
Ian Rocha <ianbritosr@gmail.com>
2019-02-01 18:43:50 -0300

    Moved telephone-bill-record logic to telephone-bill-record model

--- call_records/api/serializers.py
+++ call_records/api/serializers.py
@@ -38,74 +38,8 @@ class CallRecordSerializer(serializers.ModelSerializer):
 
         # If a pair_record was found add a record to telephone bill
         if pair_record:
-            self.insert_bill_record(validated_data, pair_record)
+            telephone_bill_obj = TelephoneBill.objects.create_bill_record(validated_data, pair_record)
 
         record = CallRecord.objects.create(**validated_data)
         record.save()
         return record
-
-    def insert_bill_record(self, validated_data, pair_record):
-        """
-        Insert a new bill record to TelephoneBill model
-        :param:
-        validated_data - The call record sent by API
-        pair_record - The pair of the call record
-        :return: Nothing
-        """
-        bill_record = TelephoneBill()
-
-        if validated_data['record_type'] == 'start':
-            # Format the bill record
-            bill_record.source = validated_data['phone_source']
-            bill_record.destination = validated_data['phone_destination']
-            bill_record.start_time = validated_data['record_timestamp'].time()
-            bill_record.start_date = validated_data['record_timestamp'].date()
-            call_start = validated_data['record_timestamp']
-            call_end = pair_record[0]['record_timestamp']
-            start_hour = validated_data['record_timestamp'].hour
-        else:
-            bill_record.source = pair_record[0]['phone_source']
-            bill_record.destination = pair_record[0]['phone_destination']
-            bill_record.start_time = pair_record[0]['record_timestamp'].time()
-            bill_record.start_date = pair_record[0]['record_timestamp'].date()
-            call_start = pair_record[0]['record_timestamp']
-            call_end = validated_data['record_timestamp']
-            start_hour = pair_record[0]['record_hour']
-
-        # Format the period of the call
-        period_month = call_end.month
-        period_year = call_end.year
-        call_period = str(period_month) + '-' + str(period_year)
-        bill_record.period = call_period
-
-        # Calculate the call duration
-        call_duration = call_end - call_start
-        bill_record.duration = call_duration
-
-        # Calculate the call price
-        duration_in_seconds = call_duration.total_seconds()
-        duration_in_minutes = int(duration_in_seconds/60)
-        call_price = self.calculate_call_price(duration_in_minutes, start_hour)
-        bill_record.price = call_price
-
-        # Save the bill record to the TelephoneBill model
-        bill_record.save()
-        return
-
-    def calculate_call_price(self, duration, start_hour):
-        """
-        Determine the price of a bill call record
-        :param:
-        duration - The duration of a call in minutes
-        start_hour - The hour that the call started
-        :return:
-        The price of a call
-        """
-        initial_price = 0.36
-        reduced_tariff = 0.0
-        standard_time = 0.09
-
-        if 6 <= start_hour < 22:
-            return initial_price + (duration * standard_time)
-        else:
-            return initial_price + (duration * reduced_tariff)
--- telephone_bill/models.py
+++ telephone_bill/models.py
@@ -2,8 +2,50 @@ from django.core.validators import RegexValidator
 from django.db import models
 
 
+class TelephoneBillManager(models.Manager):
+    def create_bill_record(self, record, pair_record):
+        if record['record_type'] == 'start':
+            # Format the bill record
+            call_start = record['record_timestamp']
+            call_end = pair_record[0]['record_timestamp']
+            start_hour = record['record_timestamp'].hour
+            # Calculate the call duration
+            call_duration = call_end - call_start
+            call_price = calculate_call_price(call_duration, start_hour)
+            # Calculate the call price
+            price = call_price
+            # Format the period of the call
+            period = format_period(call_end)
+
+            return self.model.objects.create(source=record['phone_source'],
+                                             destination=record['phone_destination'],
+                                             start_time=record['record_timestamp'].time(),
+                                             start_date=record['record_timestamp'].date(),
+                                             duration=call_duration,
+                                             price=price,
+                                             period=period)
+        else:
+            call_start = pair_record[0]['record_timestamp']
+            call_end = record['record_timestamp']
+            start_hour = pair_record[0]['record_hour']
+            # Calculate the call duration
+            call_duration = call_end - call_start
+            call_price = calculate_call_price(call_duration, start_hour)
+            # Calculate the call price
+            price = call_price
+            # Format the period of the call
+            period = format_period(call_end)
+            return self.model.objects.create(source=pair_record[0]['phone_source'],
+                                             destination=pair_record[0]['phone_destination'],
+                                             start_time=pair_record[0]['record_timestamp'].time(),
+                                             start_date=pair_record[0]['record_timestamp'].date(),
+                                             duration=call_duration,
+                                             price=price,
+                                             period=period)
+
+
 class TelephoneBill(models.Model):
-    # Simple Regex Expression for phone number validation
+    # Simple Regex Expression for period validation (MM-YYYY)
     period_validator = RegexValidator(regex=r'^(10|11|12|[1-9])[-]\d{4}$')
 
     source = models.CharField(max_length=11, blank=True, null=True)
@@ -14,8 +56,45 @@ class TelephoneBill(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     period = models.CharField(max_length=7, validators=[period_validator])
 
+    objects = TelephoneBillManager()
+
     def __str__(self):
         return 'Source: {}, Period: {}, Start Date: {}, Start Time: {}'.format(self.source,
                                                                                self.period,
                                                                                self.start_date,
                                                                                self.start_time)
+
+
+def calculate_call_price(call_duration, start_hour):
+    """
+    Determine the price of a bill call record
+    :param:
+    duration - The duration of a call in minutes
+    start_hour - The hour that the call started
+    :return:
+    The price of a call
+    """
+    initial_price = 0.36
+    reduced_tariff = 0.0
+    standard_time = 0.09
+
+    duration_in_seconds = call_duration.total_seconds()
+    duration_in_minutes = int(duration_in_seconds / 60)
+
+    if 6 <= start_hour < 22:
+        return initial_price + (duration_in_minutes * standard_time)
+    else:
+        return initial_price + (duration_in_minutes * reduced_tariff)
+
+
+def format_period(call_end):
+    """
+    :param call_end: Timestamp for the end of the call
+    :return: Period formatted MM-YYYY
+    """
+    period_month = call_end.month
+    if period_month < 10:
+        period_month = '0' + str(period_month)
+    period_year = call_end.year
+    call_period = str(period_month) + '-' + str(period_year)
+    return call_period
